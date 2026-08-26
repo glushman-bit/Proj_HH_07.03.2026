d2694c894e370e17bd8e5de6498b657549984653
Ivan Glushenkov <glushman966@gmail.com>
2026-08-26 17:32:05 +0300

    Add func read_test_data.

--- utils/read_from_file.py
+++ utils/read_from_file.py
@@ -1,12 +1,20 @@
+import json
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / "data"
 
 path_file = DATA_DIR / "companies.txt"
+test_data_file = DATA_DIR / "test_data.json"
 
 
 def read_companies_from_file(path_file):
     """Чтение списка компаний из файла."""
     with open(path_file, "r", encoding="utf-8") as f:
         return [line.strip() for line in f if line.strip()]
+
+
+def read_test_data(test_data_file):
+    """Чтение тестовых данных из JSON-файла."""
+    with open(test_data_file, "r", encoding="utf-8") as f:
+        return json.load(f)
